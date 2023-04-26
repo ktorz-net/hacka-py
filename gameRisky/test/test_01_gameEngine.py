ff7fcbecb9867e93c814fc4416ec953b5432d9a4
Guillaume Lozenguez <guillaume.lozenguez@imt-lille-douai.fr>
2023-04-26 16:07:09 +0200

    Risky board 6 and 12.

--- gameRisky/test/test_01_gameEngine.py
+++ gameRisky/test/test_01_gameEngine.py
@@ -45,11 +45,35 @@ board-4 [1, 4]
 - Cell-4 [9, 9]
 - Edge-4 [1, 2]"""
 
+#------------------------------------------------------------------------------------------------
+# Test Initialize
+#------------------------
+def test_risky_init_6():
+    game= GameRisky( 2, "board-6" )
+    assert game.map == "board-6"
+    assert game.numberOfPlayers == 2
+    game.initialize()
+    assert "\n"+ game.playerHand(1).dump() == """
+7 2 0 12 : board-6 1 6
+- 6 2 0 1 : Cell-1 5 3
+  - 1 2 0 0 : A 1 12
+- 6 4 0 0 : Edge-1 2 3 4 5
+- 6 2 0 1 : Cell-2 5 15
+  - 1 2 0 0 : B 1 12
+- 6 4 0 0 : Edge-2 1 3 4 6
+- 6 2 0 0 : Cell-3 1 9
+- 6 2 0 0 : Edge-3 1 2
+- 6 2 0 0 : Cell-4 9 9
+- 6 4 0 0 : Edge-4 1 2 5 6
+- 6 2 0 0 : Cell-5 13 3
+- 6 3 0 0 : Edge-5 1 4 6
+- 6 2 0 0 : Cell-6 13 15
+- 6 3 0 0 : Edge-6 2 4 5"""
 
 #------------------------------------------------------------------------------------------------
 # Test Initialize
 #------------------------
-def test_risky_init():
+def test_risky_init_10():
     game= GameRisky( 2, "board-10" )
     assert game.map == "board-10"
     assert game.numberOfPlayers == 2
@@ -78,6 +102,43 @@ def test_risky_init():
 - 6 2 0 0 : Edge-9 1 2
 - 7 2 0 0 : Cell-10 7 19
 - 7 6 0 0 : Edge-10 3 4 5 6 7 8"""
+
+#------------------------------------------------------------------------------------------------
+# Test Initialize
+#------------------------
+def test_risky_init_10():
+    game= GameRisky( 2, "board-12" )
+    assert game.map == "board-12"
+    assert game.numberOfPlayers == 2
+    game.initialize()
+    assert "\n"+ game.playerHand(1).dump() == """
+8 2 0 24 : board-12 1 12
+- 6 2 0 1 : Cell-1 11 3
+  - 1 2 0 0 : A 1 12
+- 6 4 0 0 : Edge-1 3 5 7 9
+- 6 2 0 1 : Cell-2 11 36
+  - 1 2 0 0 : B 1 12
+- 6 4 0 0 : Edge-2 4 6 8 10
+- 6 2 0 0 : Cell-3 15 9
+- 6 2 0 0 : Edge-3 1 11
+- 6 2 0 0 : Cell-4 15 31
+- 6 2 0 0 : Edge-4 2 11
+- 6 2 0 0 : Cell-5 7 9
+- 6 3 0 0 : Edge-5 1 9 12
+- 6 2 0 0 : Cell-6 7 30
+- 6 3 0 0 : Edge-6 2 10 12
+- 6 2 0 0 : Cell-7 11 13
+- 6 2 0 0 : Edge-7 1 12
+- 6 2 0 0 : Cell-8 11 26
+- 6 2 0 0 : Edge-8 2 12
+- 6 2 0 0 : Cell-9 1 9
+- 6 4 0 0 : Edge-9 1 5 10 12
+- 7 2 0 0 : Cell-10 1 29
+- 7 4 0 0 : Edge-10 2 6 9 12
+- 7 2 0 0 : Cell-11 15 20
+- 7 2 0 0 : Edge-11 3 4
+- 7 2 0 0 : Cell-12 7 19
+- 7 6 0 0 : Edge-12 5 6 7 8 9 10"""
 
 #------------------------------------------------------------------------------------------------
 # Test some accessors...
